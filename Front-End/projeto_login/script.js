e821f879e525d36d62639206203664b798e4e98e
Julio Cesar <juliomtd23@gmail.com>
2023-05-02 23:35:15 -0300

    ,.

--- Front-End/projeto_login/script.js
+++ Front-End/projeto_login/script.js
@@ -1,112 +1,315 @@
-/* function abrir_login(){
-    var fundo_login = document.getElementById("fundo_login")
-    fundo_login.classList.add("abrir_login")
-} */
-
-
-var fundo_login = document.getElementById("fundo_login")
-var botao_nav_login = document.getElementById("botao_nav_login")
-botao_nav_login.onclick = function() {
-    fundo_contact.close()
-    fundo_login_login.close()
-    fundo_login.show()
-}
-
-var botao_logo = document.getElementById("logo")
-botao_logo.onclick = function (){
-    fundo_login.close()
-    fundo_contact.close()
-}
-
-var botao_nav_home = document.getElementById("botao_nav_home")
-botao_nav_home.onclick = function (){
-    fundo_login.close()
-    fundo_contact.close()
-}
-
-
-var botao_nav_contact = document.getElementById("botao_nav_contact")
-var fundo_contact =  document.getElementById("fundo_contact")
-botao_nav_contact.onclick = function (){
-    fundo_login.close()
-    fundo_login_login.close()
-    fundo_contact.show()
-}
-
-var botao_register_login = document.getElementById("botao_register_login")
-var fundo_login_login = document.getElementById("fundo_login_login")
-botao_register_login.onclick = function(){
-    fundo_login.close()
-    fundo_login_login.show()
-}
-
-
-const input_username = document.getElementById("input_username");
-input_username.addEventListener("blur", function(){
-    if(input_username.value == ""){
-        label_username.classList.remove("sumir")
-        label_username.classList.add("aparecer")
-        console.log("Vazio")
-    }else{
-        const label_username = document.getElementById("label_username")
-        label_username.classList.remove("aparecer")
-        label_username.classList.add("sumir")
-        console.log("Preenchido")
+const botao_D_medicamentos = document.getElementById("botao_D_medicamentos")
+const botao_E_medicamentos = document.getElementById("botao_E_medicamentos")
+const container_slider_medicamentos = document.getElementById("container_slider_medicamentos")
+let cont_medicamentos = 0
+botao_D_medicamentos.addEventListener('click', () => {
+    if(cont_medicamentos == 0){
+        container_slider_medicamentos.style.transform="translateX(-440px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos += 1
+        console.log(cont_medicamentos)
+    }else if(cont_medicamentos == 1){
+        container_slider_medicamentos.style.transform="translateX(-760px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos += 1
+        console.log(cont_medicamentos)
+    }else if(cont_medicamentos == 2){
+        container_slider_medicamentos.style.transform="translateX(-1080px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos += 1
+        console.log(cont_medicamentos)
     }
+    else if(cont_medicamentos == 3){
+        container_slider_medicamentos.style.transform="translateX(-1400px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos += 1
+        console.log(cont_medicamentos)
+    }
+    
+})
+botao_E_medicamentos.addEventListener('click', () => {
+    if(cont_medicamentos == 0){
+        container_slider_medicamentos.style.transform="translateX(0px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+    }else if(cont_medicamentos == 1){
+        container_slider_medicamentos.style.transform="translateX(-100px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos -= 1
+        console.log(cont_medicamentos)
+    }else if(cont_medicamentos == 2){
+        container_slider_medicamentos.style.transform="translateX(-440px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos -= 1
+        console.log(cont_medicamentos)
+    }else if( cont_medicamentos == 3){
+        container_slider_medicamentos.style.transform="translateX(-760px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos -= 1
+        console.log(cont_medicamentos)
+    }
+    else if( cont_medicamentos == 4){
+        container_slider_medicamentos.style.transform="translateX(-1080px)"
+        container_slider_medicamentos.style.transition="0.5s ease-in-out"
+        cont_medicamentos -= 1
+        console.log(cont_medicamentos)
+    }
+    
 })
 
-const input_email = document.getElementById("input_email");
-input_email.addEventListener("blur", function(){
-    if(input_email.value == ""){
-        label_email.classList.remove("sumir")
-        label_email.classList.add("aparecer")
-        console.log("Vazio")
-    }else{
-        const label_email = document.getElementById("label_email")
-        label_email.classList.remove("aparecer")
-        label_email.classList.add("sumir")
-        console.log("Preenchido")
+
+
+
+
+
+
+
+
+
+const botao_D_cosmeticos = document.getElementById("botao_D_cosmeticos")
+const botao_E_cosmeticos = document.getElementById("botao_E_cosmeticos")
+const container_slider_cosmeticos = document.getElementById("container_slider_cosmeticos")
+let cont_cosmeticos = 0
+botao_D_cosmeticos.addEventListener('click', () => {
+    if(cont_cosmeticos == 0){
+        container_slider_cosmeticos.style.transform="translateX(-440px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos += 1
+        console.log(cont_cosmeticos)
+    }else if(cont_cosmeticos == 1){
+        container_slider_cosmeticos.style.transform="translateX(-760px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos += 1
+        console.log(cont_cosmeticos)
+    }else if(cont_cosmeticos == 2){
+        container_slider_cosmeticos.style.transform="translateX(-1080px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos += 1
+        console.log(cont_cosmeticos)
     }
+    else if(cont_cosmeticos == 3){
+        container_slider_cosmeticos.style.transform="translateX(-1400px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos += 1
+        console.log(cont_cosmeticos)
+    }
+    
+})
+botao_E_cosmeticos.addEventListener('click', () => {
+    if(cont_cosmeticos == 0){
+        container_slider_cosmeticos.style.transform="translateX(0px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+    }else if(cont_cosmeticos == 1){
+        container_slider_cosmeticos.style.transform="translateX(-100px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos -= 1
+        console.log(cont_cosmeticos)
+    }else if(cont_cosmeticos == 2){
+        container_slider_cosmeticos.style.transform="translateX(-440px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos -= 1
+        console.log(cont_cosmeticos)
+    }else if( cont_cosmeticos == 3){
+        container_slider_cosmeticos.style.transform="translateX(-760px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos -= 1
+        console.log(cont_cosmeticos)
+    }
+    else if( cont_cosmeticos == 4){
+        container_slider_cosmeticos.style.transform="translateX(-1080px)"
+        container_slider_cosmeticos.style.transition="0.5s ease-in-out"
+        cont_cosmeticos -= 1
+        console.log(cont_cosmeticos)
+    }
+    
 })
 
-const input_senha = document.getElementById("input_senha");
-input_senha.addEventListener("blur", function(){
-    if(input_senha.value == ""){
-        label_senha.classList.remove("sumir")
-        label_senha.classList.add("aparecer")
-        console.log("Vazio")
-    }else{
-        const label_senha = document.getElementById("label_senha")
-        label_senha.classList.remove("aparecer")
-        label_senha.classList.add("sumir")
-        console.log("Preenchido")
+
+
+
+
+
+
+
+const botao_D_ofertas = document.getElementById("botao_D_ofertas")
+const botao_E_ofertas = document.getElementById("botao_E_ofertas")
+const container_slider_ofertas = document.getElementById("container_slider_ofertas")
+let cont_ofertas = 0
+botao_D_ofertas.addEventListener('click', () => {
+    if(cont_ofertas == 0){
+        container_slider_ofertas.style.transform="translateX(-440px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas += 1
+        console.log(cont_ofertas)
+    }else if(cont_ofertas == 1){
+        container_slider_ofertas.style.transform="translateX(-760px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas += 1
+        console.log(cont_ofertas)
+    }else if(cont_ofertas == 2){
+        container_slider_ofertas.style.transform="translateX(-1080px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas += 1
+        console.log(cont_ofertas)
     }
+    else if(cont_ofertas == 3){
+        container_slider_ofertas.style.transform="translateX(-1400px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas += 1
+        console.log(cont_ofertas)
+    }
+    
+})
+botao_E_ofertas.addEventListener('click', () => {
+    if(cont_ofertas == 0){
+        container_slider_ofertas.style.transform="translateX(0px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+    }else if(cont_ofertas == 1){
+        container_slider_ofertas.style.transform="translateX(-100px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas -= 1
+        console.log(cont_ofertas)
+    }else if(cont_ofertas == 2){
+        container_slider_ofertas.style.transform="translateX(-440px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas -= 1
+        console.log(cont_ofertas)
+    }else if( cont_ofertas == 3){
+        container_slider_ofertas.style.transform="translateX(-760px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas -= 1
+        console.log(cont_ofertas)
+    }
+    else if( cont_ofertas == 4){
+        container_slider_ofertas.style.transform="translateX(-1080px)"
+        container_slider_ofertas.style.transition="0.5s ease-in-out"
+        cont_ofertas -= 1
+        console.log(cont_ofertas)
+    }
+    
 })
 
-const input_email_login = document.getElementById("input_email_login");
-input_email_login.addEventListener("blur", function(){
-    if(input_email_login.value == ""){
-        label_email_login.classList.remove("sumir")
-        label_email_login.classList.add("aparecer")
-        console.log("Vazio")
-    }else{
-        const label_email_login = document.getElementById("label_email_login")
-        label_email_login.classList.remove("aparecer")
-        label_email_login.classList.add("sumir")
-        console.log("Preenchido")
+
+
+
+
+
+
+const botao_D_conveniencia = document.getElementById("botao_D_conveniencia")
+const botao_E_conveniencia = document.getElementById("botao_E_conveniencia")
+const container_slider_conveniencia = document.getElementById("container_slider_conveniencia")
+let cont_conveniencia = 0
+botao_D_conveniencia.addEventListener('click', () => {
+    if(cont_conveniencia == 0){
+        container_slider_conveniencia.style.transform="translateX(-440px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia += 1
+        console.log(cont_conveniencia)
+    }else if(cont_conveniencia == 1){
+        container_slider_conveniencia.style.transform="translateX(-760px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia += 1
+        console.log(cont_conveniencia)
+    }else if(cont_conveniencia == 2){
+        container_slider_conveniencia.style.transform="translateX(-1080px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia += 1
+        console.log(cont_conveniencia)
     }
+    else if(cont_conveniencia == 3){
+        container_slider_conveniencia.style.transform="translateX(-1400px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia += 1
+        console.log(cont_conveniencia)
+    }
+    
+})
+botao_E_conveniencia.addEventListener('click', () => {
+    if(cont_conveniencia == 0){
+        container_slider_conveniencia.style.transform="translateX(0px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+    }else if(cont_conveniencia == 1){
+        container_slider_conveniencia.style.transform="translateX(-100px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia -= 1
+        console.log(cont_conveniencia)
+    }else if(cont_conveniencia == 2){
+        container_slider_conveniencia.style.transform="translateX(-440px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia -= 1
+        console.log(cont_conveniencia)
+    }else if( cont_conveniencia == 3){
+        container_slider_conveniencia.style.transform="translateX(-760px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia -= 1
+        console.log(cont_conveniencia)
+    }
+    else if( cont_conveniencia == 4){
+        container_slider_conveniencia.style.transform="translateX(-1080px)"
+        container_slider_conveniencia.style.transition="0.5s ease-in-out"
+        cont_conveniencia -= 1
+        console.log(conveniencia)
+    }
+    
 })
 
-const input_senha_login = document.getElementById("input_senha_login");
-input_senha_login.addEventListener("blur", function(){
-    if(input_senha_login.value == ""){
-        label_senha_login.classList.remove("sumir")
-        label_senha_login.classList.add("aparecer")
-        console.log("Vazio")
-    }else{
-        const label_senha_login = document.getElementById("label_senha_login")
-        label_senha_login.classList.remove("aparecer")
-        label_senha_login.classList.add("sumir")
-        console.log("Preenchido")
+
+
+
+
+const boinfantil = document.getElementById("botao_D_infantil")
+const botao_E_infantil = document.getElementById("botao_E_infantil")
+const container_slider_infantil = document.getElementById("container_slider_infantil")
+let cont_infantil = 0
+botao_D_infantil.addEventListener('click', () => {
+    if(cont_infantil == 0){
+        container_slider_infantil.style.transform="translateX(-440px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil += 1
+        console.log(cont_infantil)
+    }else if(cont_infantil == 1){
+        container_slider_infantil.style.transform="translateX(-760px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil += 1
+        console.log(cont_infantil)
+    }else if(cont_infantil == 2){
+        container_slider_infantil.style.transform="translateX(-1080px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil += 1
+        console.log(cont_infantil)
     }
+    else if(cont_infantil == 3){
+        container_slider_infantil.style.transform="translateX(-1400px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil += 1
+        console.log(cont_infantil)
+    }
+    
+})
+botao_E_infantil.addEventListener('click', () => {
+    if(cont_infantil == 0){
+        container_slider_infantil.style.transform="translateX(0px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+    }else if(cont_infantil == 1){
+        container_slider_infantil.style.transform="translateX(-100px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil -= 1
+        console.log(cont_infantil)
+    }else if(cont_infantil == 2){
+        container_slider_infantil.style.transform="translateX(-440px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil -= 1
+        console.log(cont_infantil)
+    }else if( cont_infantil == 3){
+        container_slider_infantil.style.transform="translateX(-760px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil -= 1
+        console.log(cont_infantil)
+    }
+    else if( cont_infantil == 4){
+        container_slider_infantil.style.transform="translateX(-1080px)"
+        container_slider_infantil.style.transition="0.5s ease-in-out"
+        cont_infantil -= 1
+        console.log(cont_infantil)
+    }
+    
 })
